da02c18b95944b5d1424ba5218da1090f02cff41
Nicolas Bazille <oltodo@msn.com>
2019-08-09 23:12:36 +0200

    Song title & subtitle with animation + refactoring

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ const typography = {
     font: "SourceSansPro-Bold",
     size: 72
   },
+  songSubtitle: {
+    font: "SourceSansPro-Regular",
+    size: 40,
+    opacity: 72
+  },
   songVerse: {
     font: "SourceSansPro-Semibold",
     size: 72
@@ -31,9 +36,8 @@ const typography = {
 };
 
 const systemEvent = Application("System Events");
-
 const keynote = Application("Keynote");
-keynote.activate();
+const mainWindow = systemEvent.processes["Keynote"].windows.byName("Test");
 
 let doc;
 if (keynote.documents.length) {
@@ -87,32 +91,161 @@ function addText(text, format) {
   textItem.objectText.font = textProperties.font;
   textItem.objectText.color = [65535, 65535, 65535];
 
+  if (textProperties.opacity) {
+    textItem.opacity = textProperties.opacity;
+  }
+
   return textItem;
+}
+
+function openInspector(tab = 0) {
+  const group = mainWindow.toolbars[0].radioGroups[0];
+  const button = group.radioButtons[tab];
+
+  if (button.value() === 0) {
+    button.click();
+    delay(0.5);
+  }
+}
+
+function openBuildOrderWindow() {
+  openInspector(1);
+  mainWindow.buttons.byName("Ordre de composition").click();
+}
+
+function getBuildOrderWindow() {
+  openBuildOrderWindow();
+
+  return systemEvent.processes["Keynote"].windows.byName(
+    "Ordre de composition"
+  );
+}
+
+function selectEffect(effect) {
+  const addEffectButton = mainWindow.buttons.byName("Ajouter un effet");
+  addEffectButton.click();
+  delay(0.2);
+  addEffectButton.popOvers[0].scrollAreas[0].buttons.byName(effect).click();
+  delay(0.2);
+}
+
+function addSongTitleEntryEffect() {
+  openInspector(1);
+  selectEffect("Fondu et déplacement");
+
+  const scrollArea = mainWindow.scrollAreas[0];
+  scrollArea.textFields[0].value = "0,7 s";
+  scrollArea.sliders[1].value = 0.1;
+
+  const popUpButton = scrollArea.popUpButtons[0];
+  popUpButton.click();
+  popUpButton.menus[0].menuItems.byName("De haut en bas").click();
+
+  const buildOrderWindow = getBuildOrderWindow();
+  buildOrderWindow.popUpButtons[0].click();
+  buildOrderWindow.popUpButtons[0].menus[0].menuItems[1].click();
+}
+
+function addSongSubtitleEntryEffect() {
+  openInspector(1);
+  selectEffect("Fondu et déplacement");
+
+  const scrollArea = mainWindow.scrollAreas[0];
+  scrollArea.textFields[0].value = "0,7 s";
+  scrollArea.sliders[1].value = 0.1;
+
+  const popUpButton = scrollArea.popUpButtons[0];
+  popUpButton.click();
+  popUpButton.menus[0].menuItems.byName("De bas en haut").click();
+
+  const buildOrderWindow = getBuildOrderWindow();
+  buildOrderWindow.popUpButtons[0].click();
+  buildOrderWindow.popUpButtons[0].menus[0].menuItems[1].click();
+  buildOrderWindow.textFields[0].value = "0,2 s";
+}
+
+function addSongTitle({
+  title,
+  copyright = "",
+  authors = "",
+  collection = ""
+}) {
+  const titleTextItem = addText(title, "songTitle");
+  const subtitle = [];
+
+  if (authors) subtitle.push(authors);
+  if (copyright) subtitle.push(`© ${copyright}`);
+  if (collection) subtitle.push(collection);
+
+  const subtitleTextItem = addText(subtitle.join(" – "), "songSubtitle");
+
+  const margin = 16;
+  const height = titleTextItem.height() + subtitleTextItem.height() + margin;
+  const y = (documentHeight - height) / 2;
+
+  titleTextItem.position = { x: titleTextItem.position().x, y };
+  addSongTitleEntryEffect();
+
+  subtitleTextItem.position = {
+    x: subtitleTextItem.position().x,
+    y: y + titleTextItem.height() + margin
+  };
+  addSongSubtitleEntryEffect();
+}
+
+function addNextSongLyricsEntryEffect() {
+  openInspector(1);
+  selectEffect("Fondu et déplacement");
+
+  const scrollArea = mainWindow.scrollAreas[0];
+  scrollArea.sliders[1].value = 0.1;
+
+  const popUpButton = scrollArea.popUpButtons[0];
+  popUpButton.click();
+  popUpButton.menus[0].menuItems.byName("De bas en haut").click();
+
+  const buildOrderWindow = getBuildOrderWindow();
+  buildOrderWindow.popUpButtons[0].click();
+  buildOrderWindow.popUpButtons[0].menus[0].menuItems[1].click();
+}
+
+function addNextSongLyrics(text, format, index, isLast) {
+  const textItem = addText(text, format);
+  textItem.width = documentWidth;
+  textItem.opacity = index ? 50 : 0;
+  textItem.position = { x: 0, y: 810 };
+
+  if (index) {
+    // Put text in background
+    press("b", { shift: true, command: true });
+  }
+
+  if (index && !isLast) {
+    addNextSongLyricsEntryEffect();
+  }
+}
+
+function addCurrentSongLyrics(text, format) {
+  const nextTextItem = addText(text, format);
+  nextTextItem.width = documentWidth;
+  nextTextItem.position = { x: 0, y: 200 };
 }
 
 function addSongSlide(song) {
   addSlide(songBackgroundsRange[0]);
-  addText(song.title, "songTitle");
+  addSongTitle(song);
 
   _.forEach(song.lyrics, (part, index) => {
     const format = part.type === "chorus" ? "songChorus" : "songVerse";
     const text = part.lines.join(`${" ".repeat(index)}\n`);
+    const isLast = index === song.lyrics.length;
 
-    const prevTextItem = addText(text, format);
-    prevTextItem.width = documentWidth;
-    prevTextItem.opacity = index ? 50 : 0;
-    prevTextItem.position = { x: 0, y: 810 };
-
-    if (index) {
-      press("b", { shift: true, command: true });
-    }
-
+    addNextSongLyrics(text, format, index, isLast);
     addSlide(songBackgroundsRange[0]);
-
-    const nextTextItem = addText(text, format);
-    nextTextItem.width = documentWidth;
-    nextTextItem.position = { x: 0, y: 200 };
+    addCurrentSongLyrics(text, format);
   });
 }
 
+keynote.activate();
+delay(0.5);
 addSongSlide(songs["a-l-ageau-de-dieu"]);
